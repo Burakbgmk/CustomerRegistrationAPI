42d66a5afc4b4364530878d577377cb72923f75b
Burakbgmk <burakbogmak@gmail.com>
2022-07-03 16:24:47 +0300

    postgresql commit

---
+++ CustomerRegistration.Data/Migrations/20220701132514_firstmig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CustomerRegistration.Data.Migrations
+{
+    public partial class firstmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Photograph = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ CustomerRegistrationAPI/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using CustomerRegistration.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerRegistration.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public CustomerController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(context.Customers.ToList());
+        }
+    }
+}
