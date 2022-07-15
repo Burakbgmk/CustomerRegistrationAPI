f8cc7dec6607c734f58cbf3fb942ab939ab70429
Burakbgmk <burakbogmak@gmail.com>
2022-07-15 18:59:49 +0300

    minor-fix2

--- CustomerRegistration.Core/Entities/BaseEntity.cs
+++ CustomerRegistration.Core/Entities/BaseEntity.cs
@@ -5,6 +5,5 @@ namespace CustomerRegistration.Core.Entities
     public class BaseEntity : IEntity
     {
         public int Id { get; set; }
-        //public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
